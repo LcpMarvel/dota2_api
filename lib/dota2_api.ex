feeeac713accd91135060916ce99dd34f6ee87b4
LcpMarvel <lcpmarvel@gmail.com>
2017-05-16 22:39:39 +0800

    init

---
+++ lib/dota2_api.ex
@@ -0,0 +1,18 @@
+defmodule Dota2API do
+  @moduledoc """
+  Documentation for Dota2 API.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Dota2API.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/dota2_api_test.exs
@@ -0,0 +1,8 @@
+defmodule Dota2APITest do
+  use ExUnit.Case
+  doctest Dota2API
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
